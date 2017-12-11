1264c2536ce624396d268780b86924c4882834f9
DESKTOP-GN5U3U3\Damian <spyque.dp@gmail.com>
2017-12-11 16:57:52 +0100

    Zmiana wygladu aplikacji pogodowej

--- app/src/main/java/com/example/damian/myapplication/MainActivity.java
+++ app/src/main/java/com/example/damian/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.damian.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button przycisk_zmiana = findViewById(R.id.przycisk_zmiana);
     }
+
+    public void click_zmiana_miejsca(View view)
+    {
+        TextView miasto = findViewById(R.id.miasto);
+        EditText podaj_nazwe = findViewById(R.id.podaj_nazwe);
+        miasto.setText(podaj_nazwe.getText().toString());
+
+    }
+
 }
